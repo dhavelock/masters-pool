87e95243777d69e3eb3d11654d35c3b2c251026f
Dylan Havelock <dylan.havelock@gmail.com>
2020-11-12 13:34:26 -0500

    added last_updated

--- api.py
+++ api.py
@@ -2,6 +2,8 @@ import flask
 from flask import request, jsonify
 from leaderboard import update_pool
 from flask_cors import CORS
+from datetime import datetime
+import pytz
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -10,9 +12,15 @@ CORS(app)
 @app.route('/api/v1/leaderboard', methods=['GET'])
 def update():
     response = {
-        'leaderboard': update_pool()
+        'leaderboard': update_pool(),
+        'last_updated': get_timestamp()
     }
     return jsonify(response)
+
+def get_timestamp():
+    fmt = '%Y-%m-%d %H:%M:%S %Z%z'
+    eastern = pytz.timezone('US/Eastern')
+    return eastern.localize(datetime.now()).strftime(fmt)
 
 if __name__ == '__main__':
     app.run()
