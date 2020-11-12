18df4f906c662975729d19ed3963da3e3283ba56
Dylan Havelock <dylan.havelock@gmail.com>
2020-11-11 19:30:29 -0500

    added cors

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
 import flask
 from flask import request, jsonify
 from leaderboard import update_pool
+from flask_cors import CORS
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+cors = CORS(app, resources={r"/api/v1/": {"origins": "http://localhost:4200/"}})
 
 @app.route('/api/v1/leaderboard', methods=['GET'])
 def update():
