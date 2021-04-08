9b9c82adb47e2cae157826f9178f11fc20e4f42d
dhavelock <dylan.havelock@gmail.com>
2021-04-08 12:12:57 -0400

    updated selections 2021

--- pool.py
+++ pool.py
@@ -1,6 +1,6 @@
 pool = [
     {
-        "full_name": "Jonathan Sharir-Smith",
+        "full_name": "JSS",
         "players": [
             {
                 "name": "Justin Thomas"
@@ -26,7 +26,7 @@ pool = [
         ]
     },
     {
-        "full_name": "Dylan Havelock",
+        "full_name": "D Picks",
         "players": [
             {
                 "name": "Justin Thomas"
@@ -48,6 +48,370 @@ pool = [
             },
             {
                 "name": "Charl Schwartzel"
+            }
+        ]
+    },
+    {
+        "full_name": "Shark",
+        "players": [
+            {
+                "name": "Justin Thomas"
+            },
+            {
+                "name": "Xander Schauffele"
+            },
+            {
+                "name": "Daniel Berger"
+            },
+            {
+                "name": "Tyrrell Hatton"
+            },
+            {
+                "name": "Victor Perez"
+            },
+            {
+                "name": "Ian Poulter"
+            },
+            {
+                "name": "Ryan Palmer"
+            }
+        ]
+    },
+    {
+        "full_name": "Trusty",
+        "players": [
+            {
+                "name": "Justin Thomas"
+            },
+            {
+                "name": "Patrick Reed"
+            },
+            {
+                "name": "Scottie Scheffler"
+            },
+            {
+                "name": "Louis Oosthuizen"
+            },
+            {
+                "name": "Corey Conners"
+            },
+            {
+                "name": "Si Woo Kim"
+            },
+            {
+                "name": "Mackenzie Hughes"
+            }
+        ]
+    },
+    {
+        "full_name": "Mr. Pistol",
+        "players": [
+            {
+                "name": "Justin Thomas"
+            },
+            {
+                "name": "Collin Morikawa"
+            },
+            {
+                "name": "Cameron Smith"
+            },
+            {
+                "name": "Tommy Fleetwood"
+            },
+            {
+                "name": "Abraham Ancer"
+            },
+            {
+                "name": "Si Woo Kim"
+            },
+            {
+                "name": "Ryan Palmer"
+            }
+        ]
+    },
+    {
+        "full_name": "Sgt. Gutmann",
+        "players": [
+            {
+                "name": "Justin Thomas"
+            },
+            {
+                "name": "Collin Morikawa"
+            },
+            {
+                "name": "Hideki Matsuyama"
+            },
+            {
+                "name": "Will Zalatoris"
+            },
+            {
+                "name": "Corey Conners"
+            },
+            {
+                "name": "Francesco Molinari"
+            },
+            {
+                "name": "Ryan Palmer"
+            }
+        ]
+    },
+    {
+        "full_name": "Ben Simmons",
+        "players": [
+            {
+                "name": "Justin Thomas"
+            },
+            {
+                "name": "Collin Morikawa"
+            },
+            {
+                "name": "Scottie Scheffler"
+            },
+            {
+                "name": "Joaquin Niemann"
+            },
+            {
+                "name": "Corey Conners"
+            },
+            {
+                "name": "Si Woo Kim"
+            },
+            {
+                "name": "Zach Johnson"
+            }
+        ]
+    },
+    {
+        "full_name": "Earle's Law",
+        "players": [
+            {
+                "name": "Rory McIlroy"
+            },
+            {
+                "name": "Tony Finau"
+            },
+            {
+                "name": "Scottie Scheffler"
+            },
+            {
+                "name": "Corey Conners"
+            },
+            {
+                "name": "Matthew Wolff"
+            },
+            {
+                "name": "Dylan Frittelli"
+            },
+            {
+                "name": "Sebastian Munoz"
+            }
+        ]
+    },
+    {
+        "full_name": "Prycey D",
+        "players": [
+            {
+                "name": "Jon Rahm"
+            },
+            {
+                "name": "Brooks Koepka"
+            },
+            {
+                "name": "Sungjae Im"
+            },
+            {
+                "name": "Louis Oosthuizen"
+            },
+            {
+                "name": "Matt Kuchar"
+            },
+            {
+                "name": "Si Woo Kim"
+            },
+            {
+                "name": "Kevin Na"
+            }
+        ]
+    },
+    {
+        "full_name": "Doc",
+        "players": [
+            {
+                "name": "Jordan Spieth"
+            },
+            {
+                "name": "Brooks Koepka"
+            },
+            {
+                "name": "Lee Westwood"
+            },
+            {
+                "name": "Tommy Fleetwood"
+            },
+            {
+                "name": "Abraham Ancer"
+            },
+            {
+                "name": "Si Woo Kim"
+            },
+            {
+                "name": "Kevin Na"
+            }
+        ]
+    },
+    {
+        "full_name": "Kelly",
+        "players": [
+            {
+                "name": "Jordan Spieth"
+            },
+            {
+                "name": "Xander Schauffele"
+            },
+            {
+                "name": "Paul Casey"
+            },
+            {
+                "name": "Tommy Fleetwood"
+            },
+            {
+                "name": "Billy Horschel"
+            },
+            {
+                "name": "Marc Leishman"
+            },
+            {
+                "name": "Zach Johnson"
+            }
+        ]
+    },
+    {
+        "full_name": "Itsy",
+        "players": [
+            {
+                "name": "Justin Thomas"
+            },
+            {
+                "name": "Tony Finau"
+            },
+            {
+                "name": "Sungjae Im"
+            },
+            {
+                "name": "Tommy Fleetwood"
+            },
+            {
+                "name": "Max Homa"
+            },
+            {
+                "name": "Cameron Champ"
+            },
+            {
+                "name": "Sebastian Munoz"
+            }
+        ]
+    },
+    {
+        "full_name": "Sarm",
+        "players": [
+            {
+                "name": "Dustin Johnson"
+            },
+            {
+                "name": "Collin Morikawa"
+            },
+            {
+                "name": "Lee Westwood"
+            },
+            {
+                "name": "Tommy Fleetwood"
+            },
+            {
+                "name": "Matthew Wolff"
+            },
+            {
+                "name": "Phil Mickelson"
+            },
+            {
+                "name": "Vijay Singh"
+            }
+        ]
+    },
+    {
+        "full_name": "Mitch",
+        "players": [
+            {
+                "name": "Dustin Johnson"
+            },
+            {
+                "name": "Patrick Cantlay"
+            },
+            {
+                "name": "Cameron Smith"
+            },
+            {
+                "name": "Tommy Fleetwood"
+            },
+            {
+                "name": "Billy Horschel"
+            },
+            {
+                "name": "Kevin Kisner"
+            },
+            {
+                "name": "Mackenzie Hughes"
+            }
+        ]
+    },
+    {
+        "full_name": "McB",
+        "players": [
+            {
+                "name": "Bryson DeChambeau"
+            },
+            {
+                "name": "Brooks Koepka"
+            },
+            {
+                "name": "Hideki Matsuyama"
+            },
+            {
+                "name": "Adam Scott"
+            },
+            {
+                "name": "Victor Perez"
+            },
+            {
+                "name": "Ian Poulter"
+            },
+            {
+                "name": "Charl Schwartzel"
+            }
+        ]
+    },
+    {
+        "full_name": "Slicer",
+        "players": [
+            {
+                "name": "Justin Thomas"
+            },
+            {
+                "name": "Collin Morikawa"
+            },
+            {
+                "name": "Sungjae Im"
+            },
+            {
+                "name": "Abraham Ancer"
+            },
+            {
+                "name": "Matt Kuchar"
+            },
+            {
+                "name": "Cameron Champ"
+            },
+            {
+                "name": "Kevin Na"
             }
         ]
     }
